bc12e6254bbba32d089bc28f722a1e4b3c4bc3ef
Deborah MARTIN <deborahmartin@macbook-pro-de-deborah.home>
2022-10-17 10:05:26 +0200

    Setup post model and posts count to discussions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_073935) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_080014) do
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
@@ -56,7 +56,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_073935) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "posts_count", default: 0
     t.index ["user_id"], name: "index_discussions_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "discussion_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["discussion_id"], name: "index_posts_on_discussion_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -77,4 +87,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_073935) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "discussions", "users"
+  add_foreign_key "posts", "discussions"
+  add_foreign_key "posts", "users"
 end
