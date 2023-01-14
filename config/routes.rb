64a90b5f3e93cc54638a74af4a2879fe777f29b6
Deborah MARTIN <deborahmartin@macbook-pro-de-deborah.home>
2023-01-14 14:51:34 +0100

    add category filtering

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   
   resources :discussions do
     resources :posts, only: [:create, :show, :edit, :update, :destroy], module: :discussions
+
+    collection do 
+      get 'category/:id', to: "categories/discussions#index", as: :category
+    end
   end
 end
